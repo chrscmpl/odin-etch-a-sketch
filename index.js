1d9640ab2424994bf4055ad435536d0d6fbe7c79
chrscmpl <chcampolongo1@gmail.com>
2022-08-03 11:44:32 +0200

    Add draw grid with custom number of squares functionality
    The grid can now be redrawn by changing the content of the number input
    below it. Each time the contents of the grid are flushed and it gets
    filled with new squares with the correct height and width so that
    the number of squares per side chosen is respected and the grid is
    filled.

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const grid = document.querySelector('#grid');
+const squaresInput = document.querySelector('#squares-input');
+const squaresStyle = document.createElement('style');
+document.head.appendChild(squaresStyle);
 
-// for (let i = 0; i < 256; i++) {
-// 	const square = document.createElement('div');
-// 	square.classList.add('square');
-// 	grid.appendChild(square);
-// }
+squaresInput.addEventListener('change', e => drawGrid(e.target.value));
+
+drawGrid(squaresInput.value); // fill when the page is loaded
+
+// takes the number of squares per side as a parameter,
+// calls resizeSquares() with the correct percentage to fill the grid completely
+// and calls fillGrid with the total number of squares (side * side)
+function drawGrid(squaresPerSide) {
+	resizeSquares(100 / squaresPerSide);
+	fillGrid(squaresPerSide * squaresPerSide);
+}
+
+// sets the height and width of elements of class square to a percentage
+function resizeSquares(percentage) {
+	squaresStyle.textContent = `.square {height: ${percentage}%; width: ${percentage}%;}`;
+}
+
+// creates n divs of class square and appends them to grid after emptying it
+function fillGrid(n) {
+	grid.textContent = '';
+	for (let i = 0; i < n; i++) {
+		const square = document.createElement('div');
+		square.classList.add('square');
+		grid.appendChild(square);
+	}
+}
